a2030bc9fc64bdc1df0043240bb379b596eb96f2
gtekle <44057863+gtekle@users.noreply.github.com>
2020-06-03 18:23:26 +0300

    Number Formatter for Currency Locale

---
+++ NumberFormatter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.text.*;
+
+public class NumberFormatter {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        String us="", india="", china="", france="";
+        NumberFormat formatterUs  = NumberFormat.getCurrencyInstance(); 
+        us = formatterUs.format(payment);
+
+        NumberFormat formatterFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        france = formatterFrance.format(payment); 
+        
+        NumberFormat formatterChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        china = formatterChina.format(payment);
+
+        NumberFormat formatterIndia = NumberFormat.getCurrencyInstance(new Locale("en", "IN")); 
+        india = formatterIndia.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
